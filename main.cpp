883bf5494dcda06a3e3792aaaf0ec39cff230fb2
Antonio Vazquez <antonio@AntonioPC.home.com>
2020-03-11 16:02:21 +0100

    Parámetros de recombinación y mutación

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@ using namespace std;
 int N; //Número de subproblemas
 int G; //Número de generaciones
 
-int opEv;//Operador evolutivo
-
+float alpha; //Parámetro de recombinación
+int recomb; //Número de valores a recombinar
+int mutar; //Número de valores a mutar
 
 int main()
 {
+    
     //Recogida de parámetros de entrada
     cout << "=====BIENVENIDO AL ALGORITMO EVOLUTIVO MULTIOBJETIVO BASADO EN AGREGACIÓN2===== \n \n";
     cout << "Indica el NÚMERO DE SUBPROBLEMAS: ";
@@ -22,14 +24,17 @@ int main()
     cin >> G;
     cout << "Ha seleccionado " << G << " generaciones.\n\n";
     
-    cout << "=AJUSTES DE EVOLUCIÓN=\n"<<endl;
-    cout << "Tipos de recombinación:"<<endl;
-    cout << "1: Recombinación aritmétrica \n2: Recombinación binaria[en desarrollo]\n"<<endl;
-    cout << "Indique el número del tipo deseado:";
-    cin >> opEv;
-    if(opEv<0 || opEv>=2){ cout << "El tipo especificado no existe"<<endl; return 1;};//Comprueba si es válido el tipo introducido
-    cout << "Ha seleccionado la recombinación tipo " << opEv<<"\n\n";
+    cout << "=AJUSTES DE EVOLUCIÓN=\n" << endl;
 
+    cout << "-Recombinación aritmétrica-" << endl;
+    cout << "Seleccione un valor alpha (a*x - (a-1)*y):";
+    cin >> alpha;
+    cout << "¿Cuántos valores se recombinan?";
+    cin >> recomb;
+
+    cout << "-Mutación-" << endl;
+    cout << "¿Cuantos valores han de mutar?(aleatoriamente)";
+    cin >> mutar;
 
 
     return 0;
