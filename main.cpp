ded4285d85a496a6d3b92bc3114c03d5d9d2e9bb
Antonio Vazquez <antonio@AntonioPC.home.com>
2020-03-22 18:56:35 +0100

    Función de recogida de parámetros

--- main.cpp
+++ main.cpp
@@ -4,38 +4,61 @@
 
 using namespace std;
 
-int N; //Número de subproblemas
-int G; //Número de generaciones
+int n; //Número de subproblemas
+int g; //Número de generaciones
 
-float alpha; //Parámetro de recombinación
-int recomb; //Número de valores a recombinar
-int mutar; //Número de valores a mutar
+float f; //Parámetro de mutación
+float cr; //Parámetro de cruce
+float vecindad;
+float ls; //Límite superior
+float li; //Límite inferior
 
+void parametrosDeEntrada();
+
+//Código principal
 int main()
 {
+    parametrosDeEntrada();
     
-    //Recogida de parámetros de entrada
-    cout << "=====BIENVENIDO AL ALGORITMO EVOLUTIVO MULTIOBJETIVO BASADO EN AGREGACIÓN2===== \n \n";
+    return 0;
+}
+
+//Recogida de parámetros de entrada
+void parametrosDeEntrada(){
+
+    cout << "=====BIENVENIDO AL ALGORITMO EVOLUTIVO MULTIOBJETIVO BASADO EN AGREGACIÓN===== \n \n";
     cout << "Indica el NÚMERO DE SUBPROBLEMAS: ";
-    cin >> N;
-    cout << "Ha seleccionado " << N << " subproblemas.\n\n";
+    cin >> n;
+    cout << "Ha seleccionado " << n << " subproblemas.\n\n";
 
     cout << "Indica el NÚMERO DE GENERACIONES: ";
-    cin >> G;
-    cout << "Ha seleccionado " << G << " generaciones.\n\n";
+    cin >> g;
+    cout << "Ha seleccionado " << g << " generaciones.\n\n";
     
     cout << "=AJUSTES DE EVOLUCIÓN=\n" << endl;
 
-    cout << "-Recombinación aritmétrica-" << endl;
-    cout << "Seleccione un valor alpha (a*x - (a-1)*y):";
-    cin >> alpha;
-    cout << "¿Cuántos valores se recombinan?";
-    cin >> recomb;
+    cout << "-Evolución diferencial-" << endl;
+    cout << "Defina el parámetro de mutación F:";
+    cin >> f;
+    cout << "Ha seleccionado " << f << " como parámetro de mutación.\n\n";
 
-    cout << "-Mutación-" << endl;
-    cout << "¿Cuantos valores han de mutar?(aleatoriamente)";
-    cin >> mutar;
+    cout << "Defina el parámetro de cruce CR:";
+    cin >> cr;
+    cout << "Ha seleccionado " << cr << " como parámetro de cruce.\n\n";
+
+    cout << "=AJUSTE DE ESPACIO DE BÚSQUEDA Y SUBPROBLEMAS=\n" << endl;
+    cout << "Defina el tamaño de vecindad de cada subproblema(porcentaje):";
+    cin >> vecindad;
+    cout << "El " << vecindad << "% de la población total formará parte de un subgrupo.\n\n";
+
+    cout << "Límite superior del subproblema:";
+    cin >> ls;
+    cout << "Ha seleccionado " << ls << " como límite superior.\n\n";
+
+    cout << "Límite inferior del subproblema:";
+    cin >> li;
+    cout << "Ha seleccionado " << li << " como límite inferior.\n\n";
 
 
-    return 0;
+
 }
